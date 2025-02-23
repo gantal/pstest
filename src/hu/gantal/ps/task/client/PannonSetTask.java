3d460c42ed0f3d606906f015c60332e6bff8bd15
gantal <antalgergely000@gmail.com>
2025-02-23 15:04:24 +0100

    add first task

---
+++ src/hu/gantal/ps/task/client/PannonSetTask.java
@@ -0,0 +1,45 @@
+package hu.gantal.ps.task.client;
+
+import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.TabPanel;
+import com.extjs.gxt.ui.client.widget.layout.FitLayout;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
+
+import hu.gantal.ps.task.client.tabitems.Feladat1;
+
+public class PannonSetTask implements EntryPoint {
+
+	private ContentPanel cp;
+	private TabPanel tp;
+
+	@Override
+	public void onModuleLoad() {
+		cp = new ContentPanel();
+		cp.setHeaderVisible(true);
+		cp.setWidth("100%");
+		cp.setFrame(true);
+		cp.setHeading("TanProjekt");
+		cp.setLayout(new FitLayout());
+
+		tp = new TabPanel();
+		tp.add(new Feladat1());		
+		cp.add(tp);
+		Window.addResizeHandler(new ResizeHandler() {
+			@Override
+			public void onResize(ResizeEvent event) {
+				resize();
+			}
+		});
+		resize();
+
+		RootPanel.get("gwt").add(cp);
+	}
+
+	private void resize() {
+		cp.setHeight(Window.getClientHeight());
+	}
+}
---
+++ src/hu/gantal/ps/task/client/tabitems/Feladat1.java
@@ -0,0 +1,19 @@
+package hu.gantal.ps.task.client.tabitems;
+
+import com.extjs.gxt.ui.client.widget.TabItem;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.google.gwt.user.client.Element;
+
+public class Feladat1 extends TabItem {
+	public Feladat1() {
+		super("Feladat1");
+	}
+
+	@Override
+	protected void onRender(Element parent, int index) {
+		super.onRender(parent, index);
+		
+		Button b = new Button("Gomb");
+		add(b);
+	}
+}
