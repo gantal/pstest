569225a223fd64cab8a7277cf6119d78bf43a825
gantal <antalgergely000@gmail.com>
2025-03-09 20:12:06 +0100

    Feladat13: add filter and clean

--- src/hu/gantal/ps/task/client/tabitems/Feladat13.java
+++ src/hu/gantal/ps/task/client/tabitems/Feladat13.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.extjs.gxt.ui.client.data.BaseDateFilterConfig;
 import com.extjs.gxt.ui.client.data.BaseFilterPagingLoadConfig;
 import com.extjs.gxt.ui.client.data.BaseModelData;
+import com.extjs.gxt.ui.client.data.BaseNumericFilterConfig;
 import com.extjs.gxt.ui.client.data.BasePagingLoader;
 import com.extjs.gxt.ui.client.data.HttpProxy;
 import com.extjs.gxt.ui.client.data.JsonPagingLoadResultReader;
 import com.extjs.gxt.ui.client.data.ListLoadResult;
+import com.extjs.gxt.ui.client.data.LoadEvent;
+import com.extjs.gxt.ui.client.data.Loader;
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.data.ModelType;
+import com.extjs.gxt.ui.client.data.PagingLoadConfig;
 import com.extjs.gxt.ui.client.data.PagingLoadResult;
 import com.extjs.gxt.ui.client.data.PagingLoader;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
@@ -28,10 +33,15 @@ import com.extjs.gxt.ui.client.widget.grid.ColumnData;
 import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
 import com.extjs.gxt.ui.client.widget.grid.Grid;
 import com.extjs.gxt.ui.client.widget.grid.GridCellRenderer;
+import com.extjs.gxt.ui.client.widget.grid.filters.DateFilter;
+import com.extjs.gxt.ui.client.widget.grid.filters.GridFilters;
+import com.extjs.gxt.ui.client.widget.grid.filters.NumericFilter;
+import com.extjs.gxt.ui.client.widget.grid.filters.StringFilter;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.toolbar.PagingToolBar;
 import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 import com.google.gwt.core.client.GWT;
+import com.extjs.gxt.ui.client.widget.grid.filters.Filter;
 import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
@@ -48,209 +58,297 @@ import hu.gantal.ps.task.client.components.EditItemWindow.SaveListener;
 import hu.gantal.ps.task.client.utils.NumberUtil;
 
 public class Feladat13 extends TabItem {
-    private static final int PAGE_SIZE = 2;
-    private static final DateTimeFormat DATE_FORMAT = DateTimeFormat.getFormat("yyyy-MM-dd");
-    private static final String DATA_URL = GWT.getHostPageBaseURL() + "feladat13_data.php";
-    private static final String CREATE_URL = GWT.getHostPageBaseURL() + "feladat13_create.php";
-    private static final String UPDATE_URL = GWT.getHostPageBaseURL() + "feladat13_update.php";
+	private static final int PAGE_SIZE = 2;
+	private static final DateTimeFormat DATE_FORMAT = DateTimeFormat.getFormat("yyyy-MM-dd");
+	private static final String DATA_URL = GWT.getHostPageBaseURL() + "feladat13_data.php";
+	private static final String CREATE_URL = GWT.getHostPageBaseURL() + "feladat13_create.php";
+	private static final String UPDATE_URL = GWT.getHostPageBaseURL() + "feladat13_update.php";
 
-    private ColumnModel cm;
-    private PagingLoader<PagingLoadResult<BaseModelData>> loader;
-    private ListStore<ModelData> store;
-    private Grid<ModelData> grid;
-    private PagingToolBar pagingToolBar;
+	private ColumnModel cm;
+	private PagingLoader<PagingLoadResult<BaseModelData>> loader;
+	private ListStore<ModelData> store;
+	private Grid<ModelData> grid;
+	private PagingToolBar pagingToolBar;
+	private GridFilters gridFilters;
 
-    public Feladat13() {
-        super("Feladat13");
-        setSize(600, 400);
-        setLayout(new FitLayout());
-        ContentPanel panel = createContentPanel();
-        add(panel);
-        loader.load();
-        layout();
-    }
+	public Feladat13() {
+		super("Feladat13");
+		setSize(600, 400);
+		setLayout(new FitLayout());
+		ContentPanel panel = createContentPanel();
+		add(panel);
+		loader.load();
+		layout();
+	}
 
-    private ContentPanel createContentPanel() {
-        ContentPanel panel = new ContentPanel();
-        panel.setHeading("Feladat13");
-        panel.setLayout(new FitLayout());
+	private ContentPanel createContentPanel() {
+		ContentPanel panel = new ContentPanel();
+		panel.setHeading("Feladat13");
+		panel.setLayout(new FitLayout());
 
-        List<ColumnConfig> columns = defineColumns();
-        cm = new ColumnModel(columns);
+		List<ColumnConfig> columns = defineColumns();
+		cm = new ColumnModel(columns);
 
-        loader = setupLoader();
-        store = new ListStore<ModelData>(loader);
-        grid = setupGrid();
+		loader = setupLoader();
+		store = new ListStore<ModelData>(loader);
+		gridFilters = setupGridFilters();
+		grid = setupGrid();
 
-        ToolBar toolBar = createToolBar();
-        pagingToolBar = new PagingToolBar(PAGE_SIZE);
-        pagingToolBar.bind(loader);
+		ToolBar toolBar = createToolBar();
+		pagingToolBar = new PagingToolBar(PAGE_SIZE);
+		pagingToolBar.bind(loader);
 
-        panel.setTopComponent(toolBar);
-        panel.setBottomComponent(pagingToolBar);
-        panel.add(grid);
+		panel.setTopComponent(toolBar);
+		panel.setBottomComponent(pagingToolBar);
+		panel.add(grid);
 
-        return panel;
-    }
+		return panel;
+	}
 
-    private List<ColumnConfig> defineColumns() {
-        List<ColumnConfig> columns = new ArrayList<ColumnConfig>();
+	private GridFilters setupGridFilters() {
+		gridFilters = new GridFilters();
+		gridFilters.addFilter(new StringFilter("product"));
+		gridFilters.addFilter(new NumericFilter("quantity"));
+		gridFilters.addFilter(new NumericFilter("price"));
+		gridFilters.addFilter(new DateFilter("orderDate"));
+
+		return gridFilters;
+	}
+
+	private String formatDateFilter(List<BaseDateFilterConfig> dateFilters) {
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < dateFilters.size(); i++) {
+			BaseDateFilterConfig filter = dateFilters.get(i);
+			Date date = (Date) filter.getValue();
+			String formattedDate = DATE_FORMAT.format(date);
+
+			if (i > 0) {
+				result.append("_");
+			}
+			result.append(filter.getComparison()).append("=").append(formattedDate);
+		}
+
+		return result.toString();
+	}
+
+	private String formatNumericFilter(List<BaseNumericFilterConfig> numericFilters) {
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < numericFilters.size(); i++) {
+			BaseNumericFilterConfig filter = numericFilters.get(i);
+			String valueString = filter.getValue().toString();
+
+			if (i > 0) {
+				result.append("_");
+			}
+			result.append(filter.getComparison()).append("=").append(valueString);
+		}
+
+		return result.toString();
+	}
+
+	private List<ColumnConfig> defineColumns() {
+		List<ColumnConfig> columns = new ArrayList<ColumnConfig>();
 		ColumnConfig idCol = new ColumnConfig("id", "Azonosító", 80);
 		idCol.setNumberFormat(NumberFormat.getFormat("#"));
 		columns.add(idCol);
-        columns.add(new ColumnConfig("product", "Termék", 150));
+		columns.add(new ColumnConfig("product", "Termék", 150));
 		ColumnConfig quantityCol = new ColumnConfig("quantity", "Mennyiség", 80);
 		quantityCol.setNumberFormat(NumberFormat.getFormat("#"));
 		columns.add(quantityCol);
-        columns.add(new ColumnConfig("price", "Egységár", 100));
+		columns.add(new ColumnConfig("price", "Egységár", 100));
 
-        ColumnConfig orderDateColumn = new ColumnConfig("orderDate", "Rendelés dátuma", 120);
-        orderDateColumn.setRenderer(new GridCellRenderer<ModelData>() {
-            @Override
-            public Object render(ModelData model, String property, ColumnData config, int rowIndex, int colIndex,
-                                 ListStore<ModelData> store, Grid<ModelData> grid) {
-                Object value = model.get(property);
-                if (value instanceof String) {
-                    try {
-                        Date parsedDate = DATE_FORMAT.parse((String) value);
-                        return DATE_FORMAT.format(parsedDate);
-                    } catch (Exception e) {
-                        return "Hibás dátum";
-                    }
-                } else if (value instanceof Date) {
-                    return DATE_FORMAT.format((Date) value);
-                }
-                return "N/A";
-            }
-        });
-        columns.add(orderDateColumn);
+		ColumnConfig orderDateColumn = new ColumnConfig("orderDate", "Rendelés dátuma", 120);
+		orderDateColumn.setRenderer(new GridCellRenderer<ModelData>() {
+			@Override
+			public Object render(ModelData model, String property, ColumnData config, int rowIndex, int colIndex,
+					ListStore<ModelData> store, Grid<ModelData> grid) {
+				Object value = model.get(property);
+				if (value instanceof String) {
+					try {
+						Date parsedDate = DATE_FORMAT.parse((String) value);
+						return DATE_FORMAT.format(parsedDate);
+					} catch (Exception e) {
+						return "Hibás dátum";
+					}
+				} else if (value instanceof Date) {
+					return DATE_FORMAT.format((Date) value);
+				}
+				return "N/A";
+			}
+		});
+		columns.add(orderDateColumn);
 
-        return columns;
-    }
+		return columns;
+	}
 
-    private PagingLoader<PagingLoadResult<BaseModelData>> setupLoader() {
-        ModelType modelType = new ModelType();
-        modelType.setRoot("data");
-        modelType.setTotalName("total");
-        modelType.addField("id", "id");
-        modelType.addField("product", "product");
-        modelType.addField("quantity", "quantity");
-        modelType.addField("price", "price");
-        modelType.addField("orderDate", "orderDate");
+	private PagingLoader<PagingLoadResult<BaseModelData>> setupLoader() {
+		ModelType modelType = new ModelType();
+		modelType.setRoot("data");
+		modelType.setTotalName("total");
+		modelType.addField("id", "id");
+		modelType.addField("product", "product");
+		modelType.addField("quantity", "quantity");
+		modelType.addField("price", "price");
+		modelType.addField("orderDate", "orderDate");
 
-        RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.GET, DATA_URL);
-        HttpProxy<String> proxy = new HttpProxy<String>(requestBuilder);
-        JsonPagingLoadResultReader<ListLoadResult<BaseModelData>> reader =
-                new JsonPagingLoadResultReader<ListLoadResult<BaseModelData>>(modelType);
+		RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.GET, DATA_URL);
+		HttpProxy<String> proxy = new HttpProxy<String>(requestBuilder);
+		JsonPagingLoadResultReader<ListLoadResult<BaseModelData>> reader = new JsonPagingLoadResultReader<ListLoadResult<BaseModelData>>(
+				modelType);
 
-        loader = new BasePagingLoader<PagingLoadResult<BaseModelData>>(proxy, reader) {
-            protected Object newLoadConfig() {
-                return new BaseFilterPagingLoadConfig();
-            }
-        };
+		loader = new BasePagingLoader<PagingLoadResult<BaseModelData>>(proxy, reader) {
+			protected Object newLoadConfig() {
+				return new BaseFilterPagingLoadConfig();
+			}
+		};
 
-        loader.setRemoteSort(true);
+		loader.setRemoteSort(true);
+		loader.addListener(Loader.BeforeLoad, new Listener<LoadEvent>() {
+		    @Override
+		    public void handleEvent(LoadEvent event) {
+		        StringBuilder sb = new StringBuilder();
+		        List<Filter> activeFilters = gridFilters.getFilterData();
+		        
+		        for (Filter filter : activeFilters) {
+		            sb.append(filter.getDataIndex()).append(":");
+		            Object filterValue = filter.getValue();
+		            String formattedValue = "";
 
-        return loader;
-    }
+		            if (filterValue instanceof List<?>) {
+		                List<?> valuesList = (List<?>) filterValue;
 
-    private Grid<ModelData> setupGrid() {
-        grid = new Grid<ModelData>(store, cm);
-        grid.setBorders(true);
-        grid.setAutoExpandColumn("product");
+		                if (!valuesList.isEmpty()) {
+		                    Object firstElement = valuesList.get(0);
 
-        grid.addListener(Events.RowDoubleClick, new Listener<GridEvent<ModelData>>() {
-            @Override
-            public void handleEvent(GridEvent<ModelData> event) {
-                ModelData selected = grid.getSelectionModel().getSelectedItem();
-                if (selected != null) {
-                    openEditWindow(selected);
-                }
-            }
-        });
+		                    if (firstElement instanceof BaseDateFilterConfig) {
+		                        formattedValue = formatDateFilter((List<BaseDateFilterConfig>) valuesList);
+		                    } else if (firstElement instanceof BaseNumericFilterConfig) {
+		                        formattedValue = formatNumericFilter((List<BaseNumericFilterConfig>) valuesList);
+		                    }
+		                }
+		            } else if (filterValue instanceof Number) {
+		                formattedValue = "eq=" + filterValue.toString();
+		            } else {
+		                formattedValue = (filterValue != null) ? filterValue.toString() : "";
+		            }
 
-        return grid;
-    }
+		            sb.append(formattedValue).append(",");
+		        }
 
-    private ToolBar createToolBar() {
-        ToolBar toolBar = new ToolBar();
+		        if (sb.length() > 0) {
+		            sb.deleteCharAt(sb.length() - 1); // Utolsó vesszőt törölni kell
+		        }
 
-        Button buttonCreate = new Button("Új", new SelectionListener<ButtonEvent>() {
-            @Override
-            public void componentSelected(ButtonEvent ce) {
-                openCreateWindow();
-            }
-        });
+		        PagingLoadConfig config = event.getConfig();
+		        config.set("filter", sb.toString());
+		    }
+		});
 
-        Button buttonDelete = new Button("Törlés", new SelectionListener<ButtonEvent>() {
-            @Override
-            public void componentSelected(ButtonEvent ce) {
-                ModelData selected = grid.getSelectionModel().getSelectedItem();
-                if (selected != null) {
-                    store.remove(selected);
-                }
-            }
-        });
 
-        toolBar.add(buttonCreate);
-        toolBar.add(buttonDelete);
+		return loader;
+	}
 
-        return toolBar;
-    }
+	private Grid<ModelData> setupGrid() {
+		grid = new Grid<ModelData>(store, cm);
+		grid.setBorders(true);
+		grid.setAutoExpandColumn("product");
 
-    private void sendDataToServer(String url, final ModelData item) {
-        RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, url);
-        builder.setHeader("Content-Type", "application/json");
+		grid.addListener(Events.RowDoubleClick, new Listener<GridEvent<ModelData>>() {
+			@Override
+			public void handleEvent(GridEvent<ModelData> event) {
+				ModelData selected = grid.getSelectionModel().getSelectedItem();
+				if (selected != null) {
+					openEditWindow(selected);
+				}
+			}
+		});
 
-        try {
-	        JSONObject jsonData = new JSONObject();
-	        jsonData.put("id", new JSONNumber(NumberUtil.getSafeInteger(item.get("id"))));
-	        jsonData.put("product", new JSONString((String) item.get("product")));
-	        jsonData.put("quantity", new JSONNumber(NumberUtil.getSafeInteger(item.get("quantity"))));
-	        jsonData.put("price", new JSONNumber(NumberUtil.getSafeDouble(item.get("price"))));
-	        jsonData.put("orderDate", new JSONString((String) item.get("orderDate")));
+		grid.addPlugin(gridFilters);
 
-            builder.sendRequest(jsonData.toString(), new RequestCallback() {
-                @Override
-                public void onResponseReceived(Request request, Response response) {
-                    if (response.getStatusCode() == 200) {
-                        loader.load();
-                    }
-                }
+		return grid;
+	}
 
-                @Override
-                public void onError(Request request, Throwable exception) {
-                    System.out.println("Hiba a szerverrel: " + exception.getMessage());
-                }
-            });
+	private ToolBar createToolBar() {
+		ToolBar toolBar = new ToolBar();
 
-        } catch (RequestException e) {
-            System.out.println("Hálózati hiba: " + e.getMessage());
-        }
-    }
+		Button buttonCreate = new Button("Új", new SelectionListener<ButtonEvent>() {
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				openCreateWindow();
+			}
+		});
 
-    private void openCreateWindow() {
-        final BaseModelData newItem = new BaseModelData();
-        newItem.set("id", store.getCount() + 1);
-        newItem.set("orderDate", DATE_FORMAT.format(new Date()));
+		Button buttonDelete = new Button("Törlés", new SelectionListener<ButtonEvent>() {
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				ModelData selected = grid.getSelectionModel().getSelectedItem();
+				if (selected != null) {
+					store.remove(selected);
+				}
+			}
+		});
 
-        EditItemWindow w = new EditItemWindow(newItem, "Új tétel hozzáadása");
-        w.setSaveListener(new SaveListener() {
-            public void onSave(ModelData updatedModel) {
-                sendDataToServer(CREATE_URL, updatedModel);
-            }
-        });
-        w.show();
-    }
+		toolBar.add(buttonCreate);
+		toolBar.add(buttonDelete);
 
-    private void openEditWindow(final ModelData item) {
-        EditItemWindow w = new EditItemWindow(item, "Tétel szerkesztése");
-        w.setSaveListener(new SaveListener() {
-            @Override
-            public void onSave(ModelData updatedModel) {
-                sendDataToServer(UPDATE_URL, updatedModel);
-            }
-        });
-        w.show();
-    }
+		return toolBar;
+	}
+
+	private void sendDataToServer(String url, final ModelData item) {
+		RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, url);
+		builder.setHeader("Content-Type", "application/json");
+
+		try {
+			JSONObject jsonData = new JSONObject();
+			jsonData.put("id", new JSONNumber(NumberUtil.getSafeInteger(item.get("id"))));
+			jsonData.put("product", new JSONString((String) item.get("product")));
+			jsonData.put("quantity", new JSONNumber(NumberUtil.getSafeInteger(item.get("quantity"))));
+			jsonData.put("price", new JSONNumber(NumberUtil.getSafeDouble(item.get("price"))));
+			jsonData.put("orderDate", new JSONString((String) item.get("orderDate")));
+
+			builder.sendRequest(jsonData.toString(), new RequestCallback() {
+				@Override
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						loader.load();
+					}
+				}
+
+				@Override
+				public void onError(Request request, Throwable exception) {
+					System.out.println("Hiba a szerverrel: " + exception.getMessage());
+				}
+			});
+
+		} catch (RequestException e) {
+			System.out.println("Hálózati hiba: " + e.getMessage());
+		}
+	}
+
+	private void openCreateWindow() {
+		final BaseModelData newItem = new BaseModelData();
+		newItem.set("id", store.getCount() + 1);
+		newItem.set("orderDate", DATE_FORMAT.format(new Date()));
+
+		EditItemWindow w = new EditItemWindow(newItem, "Új tétel hozzáadása");
+		w.setSaveListener(new SaveListener() {
+			public void onSave(ModelData updatedModel) {
+				sendDataToServer(CREATE_URL, updatedModel);
+			}
+		});
+		w.show();
+	}
+
+	private void openEditWindow(final ModelData item) {
+		EditItemWindow w = new EditItemWindow(item, "Tétel szerkesztése");
+		w.setSaveListener(new SaveListener() {
+			@Override
+			public void onSave(ModelData updatedModel) {
+				sendDataToServer(UPDATE_URL, updatedModel);
+			}
+		});
+		w.show();
+	}
 }
--- war/feladat13_data.php
+++ war/feladat13_data.php
@@ -10,17 +10,58 @@ if (PEAR::isError($db)) {
     die(json_encode(array("error" => "Kapcsolódási hiba: " . $db->getMessage())));
 }
 
-$limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 2; 
+$limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 2;
 $offset = isset($_GET['offset']) ? (int)$_GET['offset'] : 0;
 $sortField = isset($_GET['sortField']) ? $_GET['sortField'] : 'id';
 $sortDir = isset($_GET['sortDir']) && strtoupper($_GET['sortDir']) == 'DESC' ? 'DESC' : 'ASC';
 
-$totalQuery = "SELECT COUNT(*) as total FROM data";
+$filterQuery = "";
+if (isset($_GET['filter']) && !empty($_GET['filter'])) {
+    $filters = explode(",", $_GET['filter']);
+    $filterConditions = array();
+
+    foreach ($filters as $filter) {
+        list($field, $value) = explode(":", $filter, 2);
+        $field = trim($field);
+        $value = trim($value);
+
+        if (!empty($field) && !empty($value)) {
+            if (strpos($value, "lt=") !== false || strpos($value, "gt=") !== false || strpos($value, "eq=") !== false) {
+                $conditions = explode("_", $value);
+                foreach ($conditions as $condition) {
+                    if (strpos($condition, "lt=") !== false) {
+                        $number = str_replace("lt=", "", $condition);
+                        $filterConditions[] = "$field < $number";
+                    } elseif (strpos($condition, "gt=") !== false) {
+                        $number = str_replace("gt=", "", $condition);
+                        $filterConditions[] = "$field > $number";
+                    } elseif (strpos($condition, "eq=") !== false) {
+                        $number = str_replace("eq=", "", $condition);
+                        $filterConditions[] = "$field = $number";
+                    }
+                }
+            } elseif ($field == "orderDate") {
+                $value = date('Y-m-d', strtotime($value));
+                $filterConditions[] = "$field = '$value'";
+            } elseif (is_numeric($value)) {
+                $filterConditions[] = "$field = $value";
+            } else {
+                $filterConditions[] = "$field LIKE '%$value%'";
+            }
+        }
+    }
+
+    if (!empty($filterConditions)) {
+        $filterQuery = " WHERE " . implode(" AND ", $filterConditions);
+    }
+}
+
+$totalQuery = "SELECT COUNT(*) as total FROM data $filterQuery";
 $totalResult = $db->query($totalQuery);
 $totalRow = $totalResult->fetchRow(DB_FETCHMODE_ASSOC);
 $total = $totalRow['total'];
 
-$sql = "SELECT * FROM data ORDER BY $sortField $sortDir LIMIT $limit OFFSET $offset";
+$sql = "SELECT * FROM data $filterQuery ORDER BY $sortField $sortDir LIMIT $limit OFFSET $offset";
 $result = $db->query($sql);
 
 $data = array();
